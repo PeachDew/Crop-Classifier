bce997eb21fd50b778b60dbe2f76fd38790c2392
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-07 01:18:57 +0800

    added pages

--- streamlit/.ipynb_checkpoints/Crop_Spectrometer_Readings-checkpoint.py
+++ streamlit/.ipynb_checkpoints/Crop_Spectrometer_Readings-checkpoint.py
@@ -25,11 +25,19 @@ st.markdown("""
 #    logreg_model = pickle.load(file)
 with open("./pickle_objects/initdf.pkl", "rb") as file:
     df = pickle.load(file)
+    
+st.markdown("### The Data:")
+st.markdown('''
+A total of 2650 grains of barley, chickpea and sorghum cultivars were scanned using the SCIO, a recently released miniaturized NIR spectrometer. For each cultivar, 50 grains were randomly selected for scanning.
+
+Column 740 to 1070 corresponds to the SCIO wavelengths, measured in nanometers (331 variables)
+''')
+st.dataframe(df.iloc[[1,50,100,150,200,250]])
        
 
 col1, col2 = st.columns(2)
 with col1:
-    st.dataframe(df.head(20))
+    st.dataframe(df.iloc[[1,50,100,150,200,250]])
 with col2:
     st.write("Crop")
 
