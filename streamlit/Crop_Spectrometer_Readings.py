c8e1bd2c0a3f064421c0ab84882975d521f74841
PeachDew <chin.furui.raymond@dhs.sg>
2023-07-07 01:11:11 +0800

    added pages

--- streamlit/Crop_Spectrometer_Readings.py
+++ streamlit/Crop_Spectrometer_Readings.py
@@ -4,17 +4,18 @@ import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 
-'''
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.feature_selection import mutual_info_regression
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score
-'''
-
 st.set_page_config(page_title="SpeCROPmeter Readings", page_icon="🌱")
+
+#from sklearn.preprocessing import StandardScaler, MinMaxScaler
+#from sklearn.feature_selection import mutual_info_regression
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.model_selection import train_test_split
+#from sklearn.svm import SVC
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.metrics import accuracy_score, roc_auc_score
+
+
+
 
 st.markdown("""
 # Analysing Spectrometer readings of Crop variations
@@ -26,7 +27,7 @@ st.markdown("""
 #    logreg_model = pickle.load(file)
 
        
-    
+
 col1, col2 = st.columns(2)
 with col1:
     st.write("Hi!")
